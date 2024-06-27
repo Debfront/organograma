71b9e8d5e5ce6ad7454e232e88945f7e33f5b63e
Débora Monteiro <deborah_monteiro12@hotmail.com>
2024-06-27 17:00:28 -0300

    Componente Formulario

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.js'
+
+function Banner(){
+    return (
+       < header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organograma"/>
+       </header>
+    )
+}
+
+
+
+export default Banner;
