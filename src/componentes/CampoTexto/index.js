d7da8d9b8f5dd985e033357b8582dd5f8d40c2ce
Débora Monteiro <deborah_monteiro12@hotmail.com>
2024-07-02 09:24:25 -0300

    Componente Botao

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -5,13 +5,20 @@ import './CampoTexto.css'
 const CampoTexto = (props) =>{
 
     const placeholderModificada = `${props.placeholder}...`
+
+    let valor = ''
+
+    const aoDigitado = (evento) => {
+      valor = evento.target.value
+      console.log(valor)
+    }
     
     return (
         <div className="campo-texto">
         <label>
           {props.label}
         </label>
-        <input placeholder= {placeholderModificada}/>
+        <input value={valor }onChange={aoDigitado}  required={props.obrigatorio} placeholder= {placeholderModificada}/>
     </div>
   )  
 }
